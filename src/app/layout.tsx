a7c464498d2ebb9d01f1f383ad4eead33220072a
Handy Abi <denny.s4putr4@gmail.com>
2025-08-23 15:01:20 +0000

    favicon tidak jelas sama sekali , perbaiki agar terlihat iconnya. logo/i

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,8 @@ export default function RootLayout({
         <meta name="theme-color" content="#FFFFFF" />
 
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icons/icon.svg" />
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><path d=%22M4 19.5A2.5 2.5 0 0 1 6.5 17H20v60a2 2 0 0 1-2 2H6.5A2.5 2.5 0 0 1 4 76.5V19.5Z%22 fill=%22hsl(240 5.9% 10%)%22></path><path d=%22M20 17h56a2 2 0 0 1 2 2v57.5a2.5 2.5 0 0 1-2.5 2.5H20V17Z%22 fill=%22hsl(240 5.9% 10%)%22></path><path d=%22M20 17h55a2 2 0 0 1 2 2v60a2 2 0 0 1-2 2h-2V34a6 6 0 0 0-6-6H20V17Z%22 fill=%22hsl(240 5.9% 10%)%22></path><polygon points=%2220 17,20 28,31 17%22 fill=%22hsl(240 4.8% 95.9%)%22></polygon></svg>" />
+        <link rel="apple-touch-icon" href="/icon.svg" />
+        <link rel="icon" href="/icon.svg" />
         
         <script
           dangerouslySetInnerHTML={{
